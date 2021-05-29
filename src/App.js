985a1500df43f687d0b4fd5976112eb76e894443
Joseph Grant <joseph@halfcab.com.au>
2021-05-30 07:55:13 +0800

    add babel and refactor to jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "test-id" }, "Adopt Me!"),
-    React.createElement(Pet, { name: "Lady", animal: "Dog", breed: "Kelpie" }),
-    React.createElement(Pet, {
-      name: "Bungee",
-      animal: "Dog",
-      breed: "Kelpie",
-    }),
-    React.createElement(Pet, {
-      name: "Lenny",
-      animal: "Dog",
-      breed: "Australian Shepherd",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
