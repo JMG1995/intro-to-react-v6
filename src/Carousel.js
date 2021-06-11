95cf18bc0eac6d4f4ebfe6b3958d7c8edb7a31c4
Joseph Grant <joseph@halfcab.com.au>
2021-06-11 10:01:08 +0800

    add error boundary and theme contet

---
+++ src/Carousel.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: parseInt(e.target.dataset.index),
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((image, index) => (
+            <img
+              key={image}
+              src={image}
+              data-index={index}
+              onClick={this.handleIndexClick}
+              className={index === active ? "active" : ""}
+              alt="animal"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
